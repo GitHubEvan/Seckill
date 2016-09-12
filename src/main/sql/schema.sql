1a5aec92ded25caa14d477a9052039c638101ed0
githubevan <3339_hai@163.com>
2016-09-13 00:23:11 +0800

    sql schema

---
+++ src/main/sql/schema.sql
@@ -0,0 +1,32 @@
+-- 数据库 初始化
+
+--CREATE DATABASE seckill;
+
+use seckill;
+
+CREATE TABLE seckill (
+  seckill_id BIGINT NOT NULL AUTO_INCREMENT COMMENT 'product id',
+  name VARCHAR(120) NOT NULL COMMENT 'product name',
+  number INT NOT NULL COMMENT 'stock number',
+  start_time TIMESTAMP NOT NULL COMMENT 'seckill start time',
+  end_time TIMESTAMP NOT NULL COMMENT 'seckill end time',
+  create_time TIMESTAMP NOT NULL DEFAULT current_timestamp() COMMENT 'create time',
+  PRIMARY KEY (seckill_id),
+  KEY idx_start_time(start_time),
+  KEY idx_end_time(end_time),
+  KEY idx_create_time(create_time)
+)ENGINE=InnoDB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8 COMMENT 'seckill stock table';
+
+
+INSERT INTO seckill(name,number,start_time,end_time)
+    VALUES
+      ('1000元秒杀iphone6',200,'2016-09-14 00:00:00','2016-09-15 00:00:00'),
+      ('500元秒杀ipad',300,'2016-09-14 00:00:00','2016-09-15 00:00:00'),
+      ('300元秒杀红米note',1000,'2016-09-14 00:00:00','2016-09-15 00:00:00');
+
+CREATE TABLE success_killed(
+  seckill_id BIGINT NOT NULL COMMENT 'product id',
+  user_phone SMALLINT NOT NULL COMMENT 'user phone',
+  state TINYINT NOT NULL DEFAULT -1 COMMENT '-1: invalid 0:sucess 1:paied',
+  create_time TIMESTAMP NOT NULL COMMENT 'create time'
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'seckill success table'
