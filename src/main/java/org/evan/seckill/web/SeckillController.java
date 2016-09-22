fd28f535a9b75b7bd5f1b7a016bce4952c004af8
githubevan <3339_hai@163.com>
2016-09-23 00:27:10 +0800

    tt

--- src/main/java/org/evan/seckill/web/SeckillController.java
+++ src/main/java/org/evan/seckill/web/SeckillController.java
@@ -1,8 +1,12 @@
 package org.evan.seckill.web;
 
 import org.evan.seckill.dto.Exposer;
+import org.evan.seckill.dto.SeckillExcution;
 import org.evan.seckill.dto.SeckillResult;
 import org.evan.seckill.entity.Seckill;
+import org.evan.seckill.enums.SeckillStateEnum;
+import org.evan.seckill.exception.RepeatSeckillException;
+import org.evan.seckill.exception.SeckillCloseException;
 import org.evan.seckill.service.SeckillService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,13 +15,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  * Created by Evan on 9/22/2016.
  */
 @Controller
-@RequestMapping("/seckill") // url:/Ä£¿é/×ÊÔ´/{id}
+@RequestMapping("/seckill") // url:/Ä£ï¿½ï¿½/ï¿½ï¿½Ô´/{id}
 public class SeckillController {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -25,16 +30,16 @@ public class SeckillController {
     @Autowired
     private SeckillService seckillService;
 
-    @RequestMapping(name = "/list", method = RequestMethod.GET)
-    public String list(Model model) {
+    @RequestMapping(name = "/list2", method = RequestMethod.GET)
+    public String list2(Model model) {
 
         List<Seckill> seckills = seckillService.getSeckillList();
         model.addAttribute("list", seckills);
         return "list"; // WEB-INF/jsp/list.jsp
     }
 
-    @RequestMapping(value = "/seckillId/detail", method = RequestMethod.GET)
-    public String detail(@PathVariable("seckillId") long seckillId, Model mode) {
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
 
         if (null == seckillId) {
             return "redirect:/seckill/list";
@@ -44,11 +49,12 @@ public class SeckillController {
         if (null == seckill) {
             return "forword:/seckill/list";
         }
+        model.addAttribute("seckill",seckill);
         return "detail";
     }
 
     @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST,
-            produces = "{application/json;charset=UTF-8}")
+            produces = {"application/json;charset=UTF-8"})
     @ResponseBody
     public SeckillResult<Exposer> exposerUrl(@PathVariable("seckillId") long seckillId) {
 
@@ -61,5 +67,37 @@ public class SeckillController {
             result = new SeckillResult<Exposer>(false, ex.getMessage());
         }
         return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution", method = RequestMethod.POST,
+            produces = {"applicaiotn/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExcution> excutionSeckill(@PathVariable("seckillId") Long seckillId,
+                                                          @PathVariable("md5") String md5,
+                                                          @CookieValue(value = "killPhone", required = false) Long phone) {
+
+        if (null == phone) {
+            return new SeckillResult<SeckillExcution>(false, "Î´×¢ï¿½ï¿½ï¿½Ã»ï¿½");
+        }
+        try {
+            SeckillExcution seckillExcution = seckillService.executeSeckill(seckillId, phone, md5);
+            return new SeckillResult<SeckillExcution>(true, seckillExcution);
+        } catch (SeckillCloseException e) {
+            SeckillExcution seckillExcution = new SeckillExcution(seckillId, SeckillStateEnum.END);
+            return new SeckillResult<SeckillExcution>(false, seckillExcution);
+        } catch (RepeatSeckillException e) {
+            SeckillExcution seckillExcution = new SeckillExcution(seckillId, SeckillStateEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExcution>(false, seckillExcution);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            SeckillExcution seckillExcution = new SeckillExcution(seckillId, SeckillStateEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExcution>(false, seckillExcution);
+        }
+    }
+
+    @RequestMapping(value = "/time/now", method = RequestMethod.GET)
+    public SeckillResult<Long> time() {
+        Date now = new Date();
+        return new SeckillResult<Long>(true, now.getTime());
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/detail.jsp
@@ -0,0 +1,28 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Evan
+  Date: 9/22/2016
+  Time: 21:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>秒杀详情页</title>
+</head>
+<body>
+  <div class="container">
+    <div  class="panel panel-default text-center">
+      <div  class="panel-heading">
+          ${seckill.name}
+      </div>
+        <div class="panel-body"/>
+    </div>
+  </div>
+</body>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js" />
+</html>
